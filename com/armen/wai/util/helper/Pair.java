214e9326c5af57ef12393ed21eb823140c97a774
Your Name <you@example.com>
2017-02-25 01:04:05 +0400

    Add closest enemy distance

---
+++ com/armen/wai/util/helper/Pair.java
@@ -0,0 +1,23 @@
+package com.armen.wai.util.helper;
+
+/**
+ * Created by Hayk on 25/02/2017.
+ */
+public class Pair<K,V> {
+
+    private K key;
+    private V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+}
